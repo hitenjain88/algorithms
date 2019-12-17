e5c622f2a8a9169f20718b7d972e7d517e91bf66
hitenjain88 <hitenjain88@gmail.com>
2019-12-17 22:44:36 +0530

    counting sort

---
+++ sorting/countingsort.py
@@ -0,0 +1,26 @@
+def counting_sort(array):
+    low = min(array)
+    length = len(array)
+    if low < 0:
+        extra = abs(low)
+        array = [element + extra for element in array]
+
+    high = max(array)
+    count_array = [0] * (high + 1)
+
+    for item in array:
+        count_array[item] += 1
+
+    for index in range(1, len(count_array)):
+        count_array[index] += count_array[index - 1]
+
+    sorted_array = [0] * length
+
+    for element in array:
+        sorted_array[count_array[element] - 1] = element
+        count_array[element] -= 1
+
+    if low < 0:
+        sorted_array = [element - extra for element in sorted_array]
+
+    return sorted_array
