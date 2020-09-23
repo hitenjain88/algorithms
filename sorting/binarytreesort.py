182e2496673889a75d472a910c23096232598bb2
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-09-23 14:23:16 -0300

    Implements the sorting algorithm

--- sorting/binarytreesort.py
+++ sorting/binarytreesort.py
@@ -1,8 +1,8 @@
 class BinaryTree (object):
 	def __init__ (self, node):
 		self.node = node
-		self.left = left
-		self.right = right
+		self.left = None
+		self.right = None
 
 	def addChild (self, childNode):
 		if childNode == self.node:
@@ -21,3 +21,36 @@ class BinaryTree (object):
 				self.right.addChild (childNode)
 			else:
 				self.right = BinaryTree (childNode)
+
+	def sortTree (self):
+		array = []
+
+		if self.left:
+			array += self.left.sortTree ()
+
+		array.append (self.node)
+
+		if self.right:
+			array += self.right.sortTree ()
+
+		return array		
+
+def buildTree (array):
+	root = BinaryTree (array[0])
+
+	for idx in range (1, len(array)):
+		root.addChild(array[idx])
+
+	return root
+
+def main ():
+	array = [5,4,7,2,11,14,6]
+
+	tree = buildTree (array)
+
+	sortedArray = tree.sortTree()
+	
+	print (array)
+	print (sortedArray)
+
+main()
