6e6fb0abb602cb9cd78101942044628895ded55e
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-09-23 14:36:38 -0300

    Finishes the algorithm with some comments

--- sorting/binarytreesort.py
+++ sorting/binarytreesort.py
@@ -1,4 +1,6 @@
+# Tree structure
 class BinaryTree (object):
+	# Starts the Node
 	def __init__ (self, node):
 		self.node = node
 		self.left = None
@@ -8,37 +10,48 @@ class BinaryTree (object):
 		if childNode == self.node:
 			return
 
-		# Go to the left
+		# Left = less than current node
 		elif childNode < self.node:
+			# If node already has a child on the left
 			if self.left:
 				self.left.addChild (childNode)			
 			else:
 				self.left = BinaryTree (childNode)
 
-		# Go to the right
+		# Right = more than current node
 		else:
+			# If node already has a child on the right
 			if self.right:
 				self.right.addChild (childNode)
 			else:
 				self.right = BinaryTree (childNode)
 
+	# Returns a sorted array
 	def sortTree (self):
 		array = []
 
+		# if there is a left node
 		if self.left:
+			# builds array with recursion
 			array += self.left.sortTree ()
-
+	
+		# appends the "root" of left subtree 
 		array.append (self.node)
 
+		# if there is a right node
 		if self.right:
+			# builds array with recursion
 			array += self.right.sortTree ()
 
 		return array		
 
+# Returns the beginning of the Tree
 def buildTree (array):
+	# Beginning of the tree
 	root = BinaryTree (array[0])
 
+	# Adds all the childs from the array
 	for idx in range (1, len(array)):
 		root.addChild(array[idx])
-
+	
 	return root
